463a086c7724d7f08974652290119a7f4d069ee0
Jatin Patel <jatinpatel136@gmail.com>
2018-01-26 14:49:25 +0530

    Error Logging using Api for angular application

---
+++ AngularApiLogging/Controllers/LogController.cs
@@ -0,0 +1,45 @@
+﻿using AngularApiLogging.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace AngularApiLogging.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class LogController : ApiController
+    {
+        /*
+         * Write now this code directly takes error string to be logged from Frontend.
+         * We can modify our code for taking LogEntry Model and then build the error string.
+         */
+
+        // POST: api/Log
+        public bool Post([FromBody]string errorMessage)
+        {
+            // code for writing to database
+            AngularLoggingEntities angularLoggingEntities = new AngularLoggingEntities();
+            angularLoggingEntities.ApiLogs.Add(new ApiLog()
+            {
+                ErrorMessage = errorMessage
+            });
+            angularLoggingEntities.SaveChanges();
+            return true;
+        }
+
+        public bool Delete([FromBody]string errorMessage)
+        {
+            // code for writing to database
+            AngularLoggingEntities angularLoggingEntities = new AngularLoggingEntities();
+
+            var apiLogs = angularLoggingEntities.ApiLogs;
+
+            angularLoggingEntities.ApiLogs.RemoveRange(apiLogs);
+            angularLoggingEntities.SaveChanges();
+            return true;
+        }
+    }
+}
---
+++ AngularApiLogging/Models/LogEntry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularApiLogging.Models
+{
+    public enum LogLevel
+    {
+        All = 0,
+        Debug = 1,
+        Info = 2,
+        Warn = 3,
+        Error = 4,
+        Fatal = 5,
+        Off = 6
+    }
+
+    public class LogEntry
+    {
+        public DateTime Date { get; set; }
+        public string Message { get; set; }
+        public LogLevel Level { get; set; }
+        public bool LogWithDate { get; set; }
+        public string[] ExtraInfo { get; set; }
+    }
+}
